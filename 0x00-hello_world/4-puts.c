79ff6fe2188931d7b384b3ded4471a428b6d07d7
jsoganep <contact@gmail.com>
2021-02-03 23:41:47 -0500

    Corrección

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,18 @@
-#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char string[40];
+
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
+	puts(string);
+	return (0);
+}
 
