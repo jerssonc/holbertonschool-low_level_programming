7e29264978fd6ba4795c727658a2b4eecfd959ff
jsoganep <contact@gmail.com>
2021-02-04 14:33:29 -0500

    Fixed n6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,5 @@
 
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
@@ -12,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar((i % 10) + '0');
 		
