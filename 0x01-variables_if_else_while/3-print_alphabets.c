eeba4d186e31bfd9808ee7c0b9c1935f7d3041aa
jsoganep <contact@gmail.com>
2021-02-04 14:19:26 -0500

    Fixed n3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,16 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-                putchar(letter);
-        for (letter = 'A'; letter <= 'Z'; letter++)
-                putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
 
-        putchar('\n');
-        return (0);
+	return (0);
 }
 
