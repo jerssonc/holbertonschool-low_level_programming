0424ce3b93a1b02d8c2e76426a5e6de26e990ebe
jsoganep <contact@gmail.com>
2021-02-04 19:28:26 -0500

    Advanced n1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int a, i;
+
+	while (a <= 8)
+	{
+		for (a = 0; a <= 8; a++)
+		{
+			for (i = a + 1; i < 10; i++)
+			{
+				putchar((a % 10) + '0');
+				putchar((i % 10) + '0');
+				if (i > 9)
+					continue;
+				if (a == 8)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
